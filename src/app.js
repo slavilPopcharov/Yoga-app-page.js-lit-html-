f76848830f082712b3255b955426a2764c2f14a9
slavilPopcharov <slavitostz1991@gmail.com>
2022-09-10 11:18:11 +0300

    update navbar view gor users and guests

--- src/app.js
+++ src/app.js
@@ -1,27 +1,54 @@
-import { page, render} from "./libs.js";
+import { page, render } from "./libs.js";
+import { getUserData, setUserData } from "./util.js";
 import { createView } from "./views/create.js";
 import { homeView } from "./views/home.js";
 import { loginView } from "./views/login.js";
 import { registerView } from "./views/register.js"; // ADD GENDER CHOICE
 import { profileView } from "./views/userProfile.js";
 
-const main = document.getElementById('site-content');
+const main = document.getElementById("site-content");
+const logOutBtn = document.getElementById("logoutBtn");
+logOutBtn.addEventListener("click", onLogout);
 
-page(decorateContext)
-page('/', homeView);
-page('/login', loginView);
-page('/register', registerView);
-page('/create', createView);
-page('/myPoses', profileView)
+page(decorateContext);
+page("/", homeView);
+page("/login", loginView);
+page("/register", registerView);
+page("/create", createView);
+page("/myPoses", profileView);
 
+uppdateNav();
 page.start();
 
 function decorateContext(ctx, next) {
-
   ctx.render = renderMain;
+  ctx.uppdateNav = uppdateNav;
   next();
 }
 
 function renderMain(templateResult) {
-  render(templateResult, main)
+  render(templateResult, main);
+}
+
+export function uppdateNav() {
+  const userData = getUserData();
+
+  if (userData) {
+    document.querySelector("#user").style.display = "block";
+    document.querySelector("#guest").style.display = "none";
+    document.querySelector(
+      "#user span"
+    ).textContent = `Welcome ${userData.email}`;
+  } else {
+    document.querySelector("#user").style.display = "none";
+    document.querySelector("#guest").style.display = "block";
+  }
+}
+
+function onLogout() {
+  if (window.confirm("Are you shure you want to wog out?")) {
+    sessionStorage.removeItem("userData");
+    uppdateNav();
+    page.redirect("/");
+  }
 }
